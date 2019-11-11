0eb5b2554a4819cc3ff00396522b89962d1cc8b8
kevinioi <kevin.ioi@ryerson.ca>
2019-11-10 20:16:34 -0500

    reliability calcs

--- tests/determineReliability.py
+++ tests/determineReliability.py
@@ -93,7 +93,7 @@ for file_ in os.listdir("../resources//partialSnopes"):
         # break#each file
     # break#each file?
 
-with open("reliability7.txt", "w") as fp:
+with open("reliability12.txt", "w") as fp:
     for r in reliability:
         articleStances = reliability[r]
         percentCorrect = articleStances[0]/(articleStances[0]+ articleStances[1])
