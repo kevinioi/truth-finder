b1dd057f1c8d44a725e60980cfb5a486850f0e98
KevinIoi <kevin.ioi@ryerson.ca>
2019-11-22 13:45:24 -0500

    Create timeSeriesTests.py

---
+++ tests/timeSeriesTests.py
@@ -0,0 +1,46 @@
+import os 
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append('../')
+
+import json
+from liblinearpkg import *
+from liblinearpkg import liblinearutil as llu
+from util import featureBag
+import pickle
+from collections import defaultdict
+
+
+def sumStrenthOverTime():
+
+    #           [false strength, true strength]
+    strengths = [{}, {}]
+
+    for i in range(1,21):
+        strengths[0][i] = []
+        strengths[1][i] = []
+    
+    with open("../resources//timeSeries//out/fullData.json", "w") as fp:
+        fullData = json.load(fp)
+
+    for claim in fullData:
+        for day in claim[0]:
+            for article in claim[0][day]:#for each article append the reliability corrected stance probabilites
+                strengths[claim[1]][int(day)].append((article[0][2]*article[0][1],article[0][3] * article[0][1]))
+
+    #sum and print rolling probabilities
+    supportCount = 0
+    refuteCount = 0
+    supportSum = 0
+    refuteSum = 0
+    for i in range(1,21):
+        for opinion in strengths[0][i]:
+            if opinion[0] > opinion[1]:
+                refuteCount += 1
+                refuteSum += opinion[0]
+            else:
+                supportCount += 1
+                supportSum += opinion[1]
+        print(f"day {i}: support {supportSum/supportCount}  refute {refuteSum/refuteCount}")
+
+    return
